1ea8a0dac248d2bad217684ad496324d8a92e28b
XY Fan <xinyuf@arisan.io>
2024-04-11 11:56:26 +0800

    Finish contains duplicate

---
+++ contains_duplicate.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int> &nums) {
+      unordered_set<int> hash_set{};
+      for (int num: nums) {
+        if (hash_set.count(num) > 0) {
+          return true;
+        }
+        hash_set.insert(num);
+      }
+      return false;
+    }
+};
