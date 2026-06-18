df828921a024c70b662e669acbf5761a6a3ccb0f
Fan-55 <openconvenience@gmail.com>
2026-06-18 10:19:15 +0800

    Revisit move_zeros

--- move_zeros.cpp
+++ move_zeros.cpp
@@ -1,18 +1,31 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-      int l = 0;
-      for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != 0) {
-          int temp = nums[l];
-          nums[l] = nums[i];
-          nums[i] = temp;
-          l++;
-        }
+  void moveZeroes(vector<int>& nums) {
+    int index_of_swappable_zero = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums.at(i) != 0) {
+        /// swap
+        int tmp = nums.at(index_of_swappable_zero);
+        nums[index_of_swappable_zero] = nums.at(i);
+        nums[i] = tmp;
+        index_of_swappable_zero++;
       }
     }
+  }
 };
+
+int main() {
+  vector<int> nums = {0,0,1,0,3};
+  Solution s;
+  s.moveZeroes(nums);
+  cout << "mutated nums: [ ";
+  for (int num: nums) {
+    cout << num << " ";
+  }
+  cout << "]" << endl;
+}
