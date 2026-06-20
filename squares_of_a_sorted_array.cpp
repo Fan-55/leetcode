d2448e967f06c6b6b2a5bade0f50d6fc5bb3a92c
Fan-55 <openconvenience@gmail.com>
2026-06-20 23:15:26 +0800

    Revisit squares_of_a_sorted_array

--- squares_of_a_sorted_array.cpp
+++ squares_of_a_sorted_array.cpp
@@ -1,24 +1,49 @@
 #include <vector>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
-public:
-    vector<int> sortedSquares(vector<int>& nums) {
-      int l =0;
-      int r = nums.size() - 1;
-      vector<int> ans(nums.size());
-
-      for(int i = nums.size() - 1; i >= 0; i--) {
-        if (nums[l] * nums[l] >= nums[r] * nums[r]) {
-          ans[i] = nums[l] * nums[l];
-          l++;
-        } else {
-          ans[i] = nums[r] * nums[r];
-          r--;
-        }
+ public:
+  vector<int> sortedSquares(vector<int>& nums) {
+    int left_pos = 0;
+    int right_pos = nums.size() - 1;
+    vector<int> ans(nums.size());
+    for (int i = ans.size() - 1; i >= 0; i--) {
+      if (pow(nums.at(left_pos), 2) > pow(nums.at(right_pos), 2)) {
+        ans.at(i) = pow(nums.at(left_pos), 2);
+        left_pos++;
+      } else {
+        ans.at(i) = pow(nums.at(right_pos), 2);
+        right_pos--;
       }
-
-      return ans;
     }
+    return ans;
+  }
 };
+
+void test_case (vector<int>& nums) {
+  cout << " input array [ ";
+  for (int num: nums) {
+    cout << num << " ";
+  }
+  cout << "]" << endl;
+
+
+  Solution s;
+  vector<int> ans = s.sortedSquares(nums);
+  cout << " sorted squared array [ ";
+  for (int squared_num: ans) {
+    cout << squared_num << " ";
+  }
+  cout << "]" << endl;
+}
+
+int main () {
+  vector<int> nums = {-4,-1, 0, 3, 10};
+  test_case(nums);
+  nums = {0};
+  test_case(nums);
+  return 0;
+}
